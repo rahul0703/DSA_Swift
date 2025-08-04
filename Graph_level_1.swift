1542eb89fce36063d3d5b799b02c9e2c2cb2b14f
rsureka <rsureka@paypal.com>
2025-08-04 23:12:39 +0530

    Graph questions

---
+++ Graph_level_1.swift
@@ -0,0 +1,213 @@
+//
+//  Graph_level_1.swift
+//  DSA
+//
+//  Created by Rahul Sureka on 04/08/25.
+//
+
+import Foundation
+
+class Graph {
+  /*
+   Question: Get all paths from start to end in a directed graph
+   */
+  func getAllPaths(_ graph: [[Int]], _ start: Int, _ end: Int) -> [[Int]] {
+    var result: [[Int]] = []
+    var visited = Array(repeating: false, count: graph.count)
+    var path: [Int] = []
+    visited[start] = true
+    getAllPathsHelper(graph, &visited, start, end, &result, &path)
+    return result
+  }
+  
+  private func getAllPathsHelper(graph: [[Int]], visited: inout [Bool], start: Int, end: Int, result: inout [[Int]], path: inout [Int]) {
+    if start == end {
+      path.append(start)
+      result.append(path)
+      path.removeLast() // Remove the current node from path before returning
+      return
+    }
+    //Add current node to path
+    path.append(start)
+    //Call to all neighbours
+    for neighbor in graph[start] {
+      if !visited[neighbor] {
+        visited[neighbor] = true
+        getAllPathsHelper(graph: graph, visited: &visited, start: neighbor, end: end, result: &result, path: &path)
+        //Backtrack
+        visited[neighbor] = false
+      }
+    }
+    path.removeLast() // Remove the current node from path before returning
+  }
+  
+  /*
+    Question: Get connected components of the graph
+   */
+  func getConnectedComponents(_ graph: [[Int]]) -> [[Int]] {
+    var result: [[Int]] = []
+    var visited = Array(repeating: false, count: graph.count)
+    for i in 0 ..< graph.count {
+      if !visited[i] {
+        var path: [Int] = []
+        exploreComponents(graph, &visited, i, &path)
+        result.append(path) // Add the found component to the result
+      }
+    }
+    return result
+  }
+  
+  private func exploreComponents(_ graph: [[Int]], _ visited: inout [Bool], _ node: Int, _ path: inout [Int]) {
+    
+    visited[node] = true
+    path.append(node)
+    for neighbor in graph[node] {
+      if !visited[neighbor] {
+        exploreComponents(graph, &visited, neighbor, &path)
+      }
+    }
+  }
+  
+  /*
+   Question: Check if the graph is connected
+   */
+  func isGraphConnected(_ graph: [[Int]]) -> Bool {
+    var result = getConnectedComponents(graph)
+    if result.count == 1 {
+      return true // Graph is connected if there's only one component
+    } else {
+      return false
+    }
+  }
+  
+  /*
+   Question: Count number of islands in a grid
+   */
+  func countNumberOfIslands(_ grid: [[Int]]) -> Int {
+    var count = 0
+    var visited = Array(repeating: Array(repeating: false, count: grid[0].count), count: grid.count)
+    for i in 0 ..< grid.count {
+      for j in 0 ..< grid[i].count {
+        if visited[i][j] == false && grid[i][j] == 1 {
+          count += 1 // Found a new island
+          exploreIsland(grid, &visited, i, j)
+        }
+      }
+    }
+    return count
+  }
+  
+  private func exploreIsland(_ grid: [[Int]], _ visited: inout [[Bool]], _ row: Int, _ col: Int) {
+    if row < 0 || row >= grid.count || col < 0 || col >= grid[row].count || visited[row][col] || grid[row][col] == 0 {
+      return
+    }
+    visited[row][col] = true
+    //neighbour of 4 directions
+    let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    
+    for direction in directions {
+      exploreIsland(grid, &visited, row + direction.0, col + direction.1)
+    }
+  }
+  
+  /*
+   Question: Perfect friends problem
+   Desc: Given a graph, find the number of perfect friends pairs where no two friends are connected directly or indirectly.
+   */
+  func countPerfectFriendsPairs(_ graph: [[Int]]) -> Int {
+    var answer: [Int] = []
+    var visited = Array(repeating: false, count: graph.count)
+    for i in 0 ..< graph.count {
+      var  count = 0
+      if !visited[i] {
+        countPerfectFriendsPairsHelper(graph, &visited, i, &count)
+        answer.append(count) // Add the count of friends in this component
+      }
+    }
+    var totalPairs = 0
+    for i in 0 ..< answer.count {
+      for j in i + 1 ..< answer.count {
+        totalPairs += answer[i] * answer[j] // Count pairs from different components
+      }
+    }
+    return totalPairs
+  }
+  
+  private func countPerfectFriendsPairsHelper(_ graph: [[Int]], _ visited: inout [Bool], _ node: Int, _ count: inout Int) {
+    visited[node] = true
+    count += 1 // Increment the count of friends in this component
+    
+    for neighbor in graph[node] {
+      if !visited[neighbor] {
+        countPerfectFriendsPairsHelper(graph, &visited, neighbor, &count)
+      }
+    }
+  }
+  
+  /*
+   Question: Hamiltonian path problem
+   Desc: Given a graph, find if there exists a Hamiltonian path in the graph. Where Hamiltonian path is a path that visits every vertex exactly once.
+   Sol: This is very similar to getAllPaths problem, just the base case is different! There base case is if destination is reached and here along with that we also check if all nodes are visited.
+   */
+  func getAllHamiltonianPaths(_ graph: [[Int]], start: Int) -> [[Int]] {
+    var result: [[Int]] = []
+    var visited = Array(repeating: false, count: graph.count)
+    var path: [Int] = []
+    getAllHamiltonianPathHelper(graph, &visited, start, &result, &path)
+    return result
+  }
+  
+  private func getAllHamiltonianPathHelper(graph: [[Int]], visited: inout [Bool], start: Int, result: inout [[Int]], path: inout [Int]) {
+    visited[start] = true
+    path.append(start)
+    
+    if path.count == graph.count {
+      result.append(path) // Found a Hamiltonian path
+    } else {
+      for neighbor in graph[start] {
+        if !visited[neighbor] {
+          getAllHamiltonianPathHelper(graph: graph, visited: &visited, start: neighbor, result: &result, path: &path)
+        }
+      }
+    }
+    path.removeLast() // Remove the current node from path before returning
+    visited[start] = false // Backtrack
+  }
+  
+  /*
+   Question: check if hamiltonian cycle exists
+   Desc: Given a graph, find if there exists a Hamiltonian cycle in the graph, where a Hamiltonian cycle is a cycle that visits every vertex exactly once and returns to the starting vertex.
+   */
+  func isHamiltonianCycleExists(_ graph: [[Int]], start: Int) -> Bool {
+    var visited = Array(repeating: false, count: graph.count)
+    var path: [Int] = []
+    return getAllHamiltonianCycleHelper(graph, &visited, start, &path)
+  }
+  
+  private func getAllHamiltonianCycleHelper(graph: [[Int]], visited: inout [Bool], start: Int, path: inout [Int]) -> Bool {
+    visited[start] = true
+    path.append(start)
+    
+    if path.count == graph.count {
+      if graph[start].contains(path[0]) {
+        return true // Found a Hamiltonian cycle
+      }
+    } else {
+      for neighbor in graph[start] {
+        if !visited[neighbor] {
+          var answer = getAllHamiltonianCycleHelper(graph: graph, visited: &visited, start: neighbor, path: &path)
+          if answer {
+            return true // If a Hamiltonian cycle is found, return true
+          }
+        }
+      }
+    }
+    path.removeLast() // Remove the current node from path before returning
+    visited[start] = false
+    return false // No Hamiltonian cycle found in this path
+  }
+  
+}
+
+
+
--- Recursion & Arrays/Backtracking.playground/Contents.swift
+++ Recursion & Arrays/Backtracking.playground/Contents.swift
@@ -49,5 +49,123 @@ private func isSafe(matrix: [[Int]], _ row: Int, _ col: Int, _ n: Int) -> Bool {
 
 
 /*
- Question 2: 
+ Question 2: Knight tour problem.
+ Print all path from top left to bottom right of a n x n matrix. With no cell being visited twice.
  */
+let rowMoves = [2, 1, -1, -2, -2, -1, 1, 2]
+let colMoves = [1, 2, 2, 1, -1, -2, -2, -1]
+
+func printAllKnightTourPossibility(n: Int) {
+  var matrix: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
+  var visited: [[Int]] = Array(repeating: Array(repeating: 0, count: n), count: n)
+  
+  printAllKnightTourPossibility(matrix: &matrix, visited: &visited, n: n, row: 0, col: 0, pathSoFar: "")
+}
+
+private func printAllKnightTourPossibility(matrix: inout [[Int]], visited: inout [[Int]], n: Int, row: Int, col: Int, pathSoFar: String) {
+  if row < 0 || row >= n || col < 0 || col >= n || visited[row][col] == 1 {
+    return // Out of bounds or already visited
+  }
+  if row == n - 1 && col == n - 1 {
+    print(pathSoFar + "(\(row), \(col))")
+    return // Reached the bottom right corner
+  }
+  
+  visited[row][col] = 1 // Mark the cell as visited
+  matrix[row][col] = 1 // Mark the cell in the matrix
+  for i in 0 ..< 8 {
+    printAllKnightTourPossibility(matrix: &matrix, visited: &visited, n: n, row: row + rowMoves[i], col: col + colMoves[i], pathSoFar: pathSoFar + "(\(row), \(col)) ")
+  }
+  visited[row][col] = 0 // Backtrack by marking the cell as unvisited
+}
+
+
+/*
+ Question 3: Print Abbrevations
+ Print all possible abbreviations of a given word.
+ */
+func printAllAbbreviations(word: String) {
+  var length = word.count
+  var wordArray = Array(word)
+  var array = getAllAbbreviations(word: wordArray, index: 0, length: length)
+  for abbreviation in array {
+    print(abbreviation)
+  }
+}
+private func getAllAbbreviations(word: [Character], index: Int, length: Int) -> [String] {
+  var answer = [String]()
+  if index == length {
+    answer.append("")
+    return answer
+  }
+  
+  var prevArray = getAllAbbreviations(word: word, index: index + 1, length: length)
+  for abbreviation in prevArray {
+    if !abbreviation.isEmpty {
+      let firstChar = abbreviation.first!
+      if firstChar.isNumber {
+        let number = Int(String(firstChar))!
+        answer.append("\(number + 1)" + String(abbreviation.dropFirst()))
+      } else {
+        answer.append("1" + String(abbreviation))
+      }
+      answer.append(String(word[index]) + abbreviation)
+    } else {
+      answer.append(String(word[index]) + abbreviation)
+      answer.append("1" + abbreviation)
+    }
+  }
+  return answer
+}
+
+// Example usage:
+printAllAbbreviations(word: "pep")
+
+/*
+ Question 4: Joseph circles problem.
+  Desc: Given a number n, and a number k, find the last person remaining in the circle after repeatedly removing every k-th person.
+ Approch: We will use linked list to solve this problem as it is efficient for removing elements.
+ */
+class Node {
+  var value: Int
+  var next: Node?
+  var prev: Node?
+  
+  init(value: Int) {
+    self.value = value
+  }
+}
+
+func josephusProblem(n: Int, k: Int) -> Int {
+  if n == 0 { return 0 }
+  let head = Node(value: 1)
+  var prev = head
+  for i in 2...n {
+    let node = Node(value: i)
+    prev.next = node
+    node.prev = prev
+    prev = node
+  }
+  prev.next = head
+  head.prev = prev
+  
+  var current = head
+  var size = n
+  while size > 1 {
+    for _ in 1..<k {
+      current = current.next!
+    }
+    // Remove current
+    current.prev?.next = current.next
+    current.next?.prev = current.prev
+    current = current.next!
+    size -= 1
+  }
+  return current.value
+}
+
+/*
+ 
+ */
+
+
